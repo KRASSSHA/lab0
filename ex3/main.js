6c1a63a7e40e9366802bb290741fee5cc9c823b0
SASHA <sxiruna@yandex.ru>
2025-02-24 16:10:11 +0000

    js fix

--- ex3/main.js
+++ ex3/main.js
@@ -2,5 +2,5 @@ conf = document.querySelector(".conf");
 result = document.querySelector(".result");
 R = document.querySelector('[name="R"]');
 conf.addEventListener("click", function(){
-    result.textContent = (7.44 * parseInt(R.value) ** 3).toFixed(2)
+    result.textContent = (7.66 * parseInt(R.value) ** 3).toFixed(2)
 });
